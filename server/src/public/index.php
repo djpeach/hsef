58b2730e4964f3551c40fee5b4324072dfaea87d
Daniel Peach <dpeaches96@gmail.com>
2020-09-04 19:46:50 -0400

    new structure

--- server/src/public/index.php
+++ server/src/public/index.php
@@ -4,8 +4,8 @@ require '../../vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/', function() {
-  echo 'Hello World!';
+$app->get('/', function() use ($app) {
+  $app->response->write('Welcome');
 });
 
 $app->run();
