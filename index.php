16a0532b638bf4aea4cb964ff0159aa40fc20785
Daniel Peach <dpeaches96@gmail.com>
2020-09-14 09:52:41 -0400

    remove menu when unauthed

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
 <?php require 'parts/head.php'; ?>
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/hsef/helpers/fallback.php'; ?>
 
 <div class="lab1">
   <nav class="navbar navbar-dark navbar-custom bg-dark">
     <a href="/hsef/?page=dashboard" class="navbar-brand">HSEF System</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="mainMenu">
-      <div class="navbar-nav">
-        <span class="navbar-text">Owner Tools</span>
-        <a href="/hsef/?page=adminManagement" class="nav-item nav-link">Admin Management</a>
-        <span class="navbar-text">Admin Tools</span>
-        <a href="/hsef/?page=judgeManagement" class="nav-item nav-link">Judge Management</a>
-        <a href="/hsef/?page=studentManagement" class="nav-item nav-link">Student Management</a>
-        <a href="/hsef/?page=projectManagement" class="nav-item nav-link">Project Management</a>
-        <a href="/hsef/?page=scoreManagement" class="nav-item nav-link">Score Management</a>
-        <?php if (isset(Post::get()->LOGOUT)) {
-          $authAccount->logout();
-          redirect('login');
-        } ?>
-        <?php if ($authAccount->isAuthenticated()) : ?>
-        <form method="POST" class="form-inline">
-          <button class="btn btn-lg btn-warning" type="submit" name="LOGOUT">Logout</button>
-        </form>
-        <?php endif; ?>
+    <?php if ($authAccount->isAuthenticated()) : ?>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="mainMenu">
+        <div class="navbar-nav">
+          <span class="navbar-text">Owner Tools</span>
+          <a href="/hsef/?page=adminManagement" class="nav-item nav-link">Admin Management</a>
+          <span class="navbar-text">Admin Tools</span>
+          <a href="/hsef/?page=judgeManagement" class="nav-item nav-link">Judge Management</a>
+          <a href="/hsef/?page=studentManagement" class="nav-item nav-link">Student Management</a>
+          <a href="/hsef/?page=projectManagement" class="nav-item nav-link">Project Management</a>
+          <a href="/hsef/?page=scoreManagement" class="nav-item nav-link">Score Management</a>
+          <?php if (isset(Post::get()->LOGOUT)) {
+            $authAccount->logout();
+            redirect('login');
+          } ?>
+          <form method="POST" class="form-inline">
+            <button class="btn btn-lg btn-warning" type="submit" name="LOGOUT">Logout</button>
+          </form>
+        </div>
       </div>
-    </div>
+    <?php endif; ?>
   </nav>
 
   <?php if (isset($session->flashMessage)) : ?>
