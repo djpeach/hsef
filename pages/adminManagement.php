ae97d100eb56183574543f925f74253bcb6ce791
Ashley Harris <ashleylaneharris@gmail.om>
2020-09-18 14:59:58 -0400

    fixed accidental typing

--- pages/adminManagement.php
+++ pages/adminManagement.php
@@ -1,6 +1,4 @@
--+
-
-23.0<?php if (!Operator::get()->hasOneOfReqEntitlement(['owner', 'moderator'])) {
+<?php if (!Operator::get()->hasOneOfReqEntitlement(['owner', 'moderator'])) {
   redirect('exception', 'You do not have permission to view this page');
   die();
 } ?>
