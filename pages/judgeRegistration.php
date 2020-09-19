490622cd1b4d9df13d447c8eda9a6320b51bf76a
Daniel Peach <dpeaches96@gmail.com>
2020-09-19 11:16:47 -0400

    registered success message

--- pages/judgeRegistration.php
+++ pages/judgeRegistration.php
@@ -4,6 +4,7 @@
     <?php
     $existingUser = false;
     $allowUserSelect = false;
+    $registered = false;
 
     $readonly = isset($_GET['readonly']) ? $_GET['readonly'] === 'true' : false;
 
@@ -32,103 +33,49 @@
       }
 
       if ($errors->isEmpty()) {
-        $db = DB::get();
-//            $opid =$existingUser ? $_GET['opid'] : null;
-//            if ($existingUser) { // edited existing admin
-//              // update user details
-//              $sql = $db->prepare(Queries::UPDATE_USER_BY_OPID);
-//              $sql->execute([$post->firstName, $post->lastName, $post->suffix, $post->email, $opid]);
-//              // update operator details
-//              $sql = $db->prepare(Queries::UPDATE_OPERATOR_BY_ID);
-//              $sql->execute([$post->title, $post->highestDegree, $opid]);
-//            } else { // new admin
-//              if ($didSelectUser) { // created new admin from existing user
-//                // make operator an admin
-//                $sql = $db->prepare(Queries::NEW_ADMIN_BY_UID);
-//                $sql->execute([$post->userId]);
-//                // update operator details
-//                $sql = $db->prepare(Queries::UPDATE_OPERATOR_BY_UID);
-//                $sql->execute([$post->title, $post->highestDegree, $post->userId]);
-//                // get Operator Id
-//                $sql = $db->prepare(Queries::GET_OPERATOR_BY_UID);
-//                $sql->execute([$post->userId]);
-//                $opid = $sql->fetch()->OperatorId;
-//              } else if (!$didSelectUser && !$existingUser) { // created new admin with new user details
-//                // create new user
-//                $sql = $db->prepare(Queries::CREATE_NEW_USER_WITH_EMAIL);
-//                $suffix = $post->suffix === '' ? null : $post->suffix;
-//                $sql->execute([$post->firstName, $post->lastName, $suffix, $post->email]);
-//                $uid = $db->lastInsertId();
-//                // create new operator
-//                $sql = $db->prepare(Queries::CREATE_NEW_OPERATOR_WITH_UID);
-//                $title = $post->title === '' ? null : $post->title;
-//                $highestDegree = $post->highestDegree === '' ? null : $post->highestDegree;
-//                $sql->execute([$uid, $title, $highestDegree]);
-//                $opid = $db->lastInsertId();
-//                // make new operator an admin
-//                $sql = $db->prepare(Queries::NEW_ADMIN_BY_UID);
-//                $sql->execute([$uid]);
-//              }
-//              // check for auth account
-//              $sql = $db->prepare(Queries::GET_AUTHACCOUNT_BY_OPID);
-//              $sql->execute([$opid]);
-//
-//              if ($sql->fetch()) {
-//                // TODO: email existing user they have been made an admin
-//              } else {
-//                $sql = $db->prepare(Queries::GET_USER_BY_OPID);
-//                $sql->execute([$opid]);
-//                $user = $sql->fetch();
-//                $pwd = AuthAccount::generateAccountWithUserId($user->UserId);
-//                // TODO: email user their new auth credentials
-//              }
-//
-//            }
-//            redirect('adminForm', [['opid', $opid], ['readonly', 'true']]);
-        $readonly = true;
+        // do db work
+        $registered = true;
       }
     }
     ?>
-    <h2 class="article-header">Judge Registration</h2>
-    <?php include 'components/divider.php' ?>
-    <form method="POST" class="container">
-      <?php include_once 'pages/userFields.php'?>
-      <fieldset <?php echo $readonly ? 'disabled' : ''; ?>>
-        <div class="row mt-3">
-          <div class="col">
-            <div class="floating-label-group">
-              <input type="text" placeholder="Title" id="title" name="title" value="<?php echo $post->title ?>">
-              <label for="title">Title</label>
-              <p class="form-error"><?php echo $errors->title; ?></p>
+    <?php if ($registered) : ?>
+      <h2 class="article-header">Registered!</h2>
+      <?php include 'components/divider.php' ?>
+      <div class="alert alert-darkgreen mt-5" role="alert">
+        <h2 class="alert-heading">Thank you!</h2>
+        <hr>
+        <p>You have been submitted for approval to judge <?php date('Y') ?> Hoosier Science and Engineering Fair. Read more about us
+          <a href="http://sefi.org/hsef/" class="alert-link">here</a></p>
+        <hr>
+        <p class="mb-0">Be on a lookout for an acceptance email from <?php echo $rootEmail; ?> with the subject "Approved to Judge HSEF".</p>
+      </div>
+    <?php else : ?>
+      <h2 class="article-header">Judge Registration</h2>
+      <?php include 'components/divider.php' ?>
+      <form method="POST" class="container">
+        <?php include_once 'pages/userFields.php'?>
+        <fieldset>
+          <div class="row mt-3">
+            <div class="col">
+              <div class="floating-label-group">
+                <input type="text" placeholder="Title" id="title" name="title" value="<?php echo $post->title ?>">
+                <label for="title">Title</label>
+                <p class="form-error"><?php echo $errors->title; ?></p>
+              </div>
+            </div>
+            <div class="col">
+              <div class="floating-label-group">
+                <input type="text" placeholder="Highest Degree" id="highestDegree" name="highestDegree" value="<?php echo $post->highestDegree ?>">
+                <label for="highestDegree">Highest Degree Earned</label>
+                <p class="form-error"><?php echo $errors->highestDegree; ?></p>
+              </div>
             </div>
           </div>
-          <div class="col">
-            <div class="floating-label-group">
-              <input type="text" placeholder="Highest Degree" id="highestDegree" name="highestDegree" value="<?php echo $post->highestDegree ?>">
-              <label for="highestDegree">Highest Degree Earned</label>
-              <p class="form-error"><?php echo $errors->highestDegree; ?></p>
-            </div>
-          </div>
-        </div>
-        <?php if (!$readonly) : ?>
           <div class="row mt-3">
             <button class="ml-auto btn btn-darkgreen" type="submit" name="JUDGE_REGISTRATION">Submit Registration</button>
           </div>
-        <?php endif; ?>
-      </fieldset>
-      <?php if ($readonly) : ?>
-        <fieldset>
-          <div class="row mt-3">
-            <?php
-            $href = '/hsef/?page=judgeRegistration';
-            ?>
-            <a href="/hsef/?page=judgeRegistration&readonly=false" class="btn btn-darkgreen ml-auto">
-              <i class="fas fa-edit text-white"></i>
-              Edit Details
-            </a>
-          </div>
         </fieldset>
-      <?php endif; // TODO: do the edit/readonly toggle with javascript, not a redirect ?>
-    </form>
+      </form>
+    <?php endif; ?>
   </article>
 </main>
--- parts/head.php
+++ parts/head.php
@@ -15,9 +15,12 @@
   require_once 'helpers/utils.php';
   require_once 'helpers/Exceptions.php';
 
-ini_set('variables_order', 'EGPCS');
-ini_set('session.cookie_lifetime', 86400);
-ini_set('session.name', 'hsef');
+  ini_set('variables_order', 'EGPCS');
+  ini_set('session.cookie_lifetime', 86400);
+  ini_set('session.name', 'hsef');
+
+  // TODO get the actual email for sending emails
+  $rootEmail = "hsef@sefi.org";
 
   /**
    * Initialize authentication
