8498506e6eb89543e6f09057ac06ac559be06df9
Daniel Peach <dpeaches96@gmail.com>
2020-09-25 00:40:13 -0400

    new divider icon

--- components/divider.php
+++ components/divider.php
@@ -1,7 +1,7 @@
 <div class="custom-divider">
   <div class="custom-divider__line"></div>
   <div class="custom-divider__icon">
-    <i class="fas fa-star"></i>
+    <i class="fas fa-atom"></i>
   </div>
   <div class="custom-divider__line"></div>
 </div>
