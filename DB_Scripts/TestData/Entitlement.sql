5d6c15395851d8e3de260f186967cbdfa109350f
Daniel Peach <dpeaches96@gmail.com>
2020-10-08 17:02:01 -0400

    mockeroo data

---
+++ DB_Scripts/TestData/Entitlement.sql
@@ -0,0 +1,4 @@
+insert into Entitlement (Name) values ('owner');
+insert into Entitlement (Name) values ('admin');
+insert into Entitlement (Name) values ('judge');
+insert into Entitlement (Name) values ('auditor');
