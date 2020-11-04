403b871b4a7f241059caa822d63b7653579c5757
Kate Davis <daviskate84@gmail.com>
2020-11-04 18:18:30 -0500

    adding files

---
+++ client/src/views/uploadCSV.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <p>Upload Student and Project Information</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  name: 'uploadCSV',
+  data: () => ({
+    // ...
+  }),
+}
+</script>
