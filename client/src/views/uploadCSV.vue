f833d1914877d2f32bf11c6122a7e4648d1c58d2
Kate Davis <daviskate84@gmail.com>
2020-11-05 14:30:50 -0500

    Added upload pages and logic

--- client/src/views/uploadCSV.vue
+++ client/src/views/uploadCSV.vue
@@ -1,40 +1,36 @@
 <template>
-  <v-container>
+    <v-container>
     <v-row>
       <v-col>
         <p>Upload Student and Project Information</p>
       </v-col>
+      <div id="app">
+  <v-app id="inspire">
+    <v-file-input
+      accept="image/*"
+      label="File input"
+    ></v-file-input>
+    <v-row
+      align="center"
+      justify="space-around"
+    >
+      <v-row>
+      <v-btn color="amber" depressed>
+        Upload
+      </v-btn>
+       </v-row>
     </v-row>
-    <form enctype="multipart/form-data" method="post" action="uploadStudent.php">
-    <table border="1">
-    <tr >
-    <td colspan="2" align="center"><strong>Step 1: Upload Students</strong></td>
-    </tr>
-    <tr>
-    <td align="center">File:</td><td><input type="file" name="file" id="file"></td></tr>
-    <tr >
-    <td colspan="2" align="center"><input type="submit" value="Upload"></td>
-    </tr>
-    </table>
-    </form>
-        <form enctype="multipart/form-data" method="post" action="uploadProject.php">
-    <table border="1">
-    <tr >
-    <td colspan="2" align="center"><strong>Step 2: Upload Projects</strong></td>
-    </tr>
-    <tr>
-    <td align="center">File:</td><td><input type="file" name="file" id="file"></td></tr>
-    <tr >
-    <td colspan="2" align="center"><input type="submit" value="Upload"></td>
-    </tr>
-    </table>
-    </form>
-  </v-container>   
+  </v-app>
+</div>
+      
+    </v-row>
+  </v-container> 
 </template>
 
 <script>
 export default {
   name: 'uploadCSV',
+  el: '#app',
   data: () => ({
     // ...
   }),
