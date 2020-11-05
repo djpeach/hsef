68e2095bb37c263022734097cff2a1440710cbb6
Kate Davis <daviskate84@gmail.com>
2020-11-05 16:14:23 -0500

    added links

--- api/controllers/uploadProject.php
+++ api/controllers/uploadProject.php
@@ -14,8 +14,7 @@ if($ext=="csv")
   $file = fopen($filename, "r");
          while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE)
          {
-             //We may have to change the heaaders in the strucutre to be StudentNumber and StudentName so we don't conflict with key words?
-            $sql = "INSERT into Project(ProjectId,StudentNumber,StudentName,Abstract,BoothId,CourseNetworkingId,CategoryId) values('$emapData[0]','$emapData[1]','$emapData[2],$emapData[3]','$emapData[4]','$emapData[5]','$emapData[6]')";
+            $sql = "INSERT into Project(ProjectId,Number,Name,Abstract,BoothId,CourseNetworkingId,CategoryId) values('$emapData[0]','$emapData[1]','$emapData[2],$emapData[3]','$emapData[4]','$emapData[5]','$emapData[6]')";
             mysqli_query($con, $sql);
          }
          fclose($file);
