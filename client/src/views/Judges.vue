c0ae913e2044b857c5d584732a86bf7fea817855
Ashley Harris <ashleylaneharris@gmail.om>
2020-11-05 21:41:03 -0500

    added to index.js and others

--- client/src/views/Judges.vue
+++ client/src/views/Judges.vue
@@ -9,10 +9,32 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   name: 'Judges',
   data: () => ({
-    // ...
+
   }),
+  computed: {
+    ...mapState({
+      judges: state => state.judges
+    })
+  },
+  methods: {
+    ...mapActions({
+      getJudges: 'refreshActiveJudges'
+    })
+  },
+  mounted() {
+    this.getJudges({
+      limit: 10,
+      offset: 0
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 }
 </script>
