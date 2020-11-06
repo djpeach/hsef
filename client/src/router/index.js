355cf3f7b2ea1a733b4cb40d74ba831238e38125
daniel <danmannorth21@gmail.com>
2020-11-05 19:05:22 -0500

    imported components

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,6 +8,16 @@ import FinalScores from "@/views/FinalScores";
 import UploadCSV from "@/views/UploadCSV";
 import store from '../store';
 import LogIn from "@/views/LogIn";
+import Students from "@/views/Students";
+import Projects from "@/views/Projects";
+import Schools from "@/views/Schools";
+import Categories from "@/views/Categories";
+import Scores from "@/views/Scores";
+import Booths from "@/views/Booths";
+import Counties from "@/views/Counties";
+import GradeLevels from "@/views/GradeLevels";
+import Judges from "@/views/Judges";
+
 
 Vue.use(VueRouter)
 
@@ -34,6 +44,15 @@ const routes = [
   { path: '/', name: 'dashboard', component: Dashboard },
   { path: '/admin-management', name: 'admin-management', component: AdminManagement },
   { path: '/event-management', name: 'event-management', component: EventManagement },
+  { path: '/judges', name: 'judges', component: Judges },
+  { path: '/students', name: 'students', component: Students },
+  { path: '/projects', name: 'projects', component: Projects },
+  { path: '/schools', name: 'schools', component: Schools },
+  { path: '/categories', name: 'categories', component: Categories },
+  { path: '/scores', name: 'scores', component: Scores },
+  { path: '/booths', name: 'booths', component: Booths },
+  { path: '/counties', name: 'counties', component: Counties },
+  { path: '/grade-levels', name: 'grade-levels', component: GradeLevels },
   { path: '/judge-schedule', name: 'judge-schedule', component: JudgeSchedule },
   { path: '/final-scores', name: 'final-scores', component: FinalScores },
   { path: '/upload-csv', name: 'upload-csv', component: UploadCSV },
