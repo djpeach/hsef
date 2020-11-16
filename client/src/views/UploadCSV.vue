5bb9a42a33f740d554a1f2873e2ca11452289a11
Ashley Harris <ashleylaneharris@gmail.om>
2020-11-16 14:25:38 -0500

    added sample CSV to Assets folder, added button to download sample CSV to UploadCSV.vue, work in progreess

--- client/src/views/UploadCSV.vue
+++ client/src/views/UploadCSV.vue
@@ -8,11 +8,27 @@
             accept="csv/*"
             label="File input"
         ></v-file-input>
-      <form action="uploadCSV.php" method="post">
-        <v-btn color="amber" class="submit" depressed>
-          Upload
-        </v-btn>
-      </form>
+      </v-col>
+    </v-row>
+
+    <v-row align="center"
+      justify="space-around">
+      <v-col>
+        <!--<form method="get" action="client/src/assets/studentBulkUpload.xls">
+          <v-btn type="submit">Sample CSV Download</v-btn>
+        </form>-->
+        <a href='client/src/assets/studentBulkUpload.xls' target="_blank">Download</a>
+          <!--<a style="text-decoration-line: none" href="client/src/assets/studentBulkUpload.xls" download>
+            <v-btn>Sample CSV Download</v-btn>
+          </a>-->
+      </v-col>
+      <v-spacer> </v-spacer>
+      <v-col>
+        <form action="uploadCSV.php" method="post">
+          <v-btn color = "amber" style="float: right" class="submit" depressed>
+            Upload
+          </v-btn>
+        </form>
       </v-col>
       
     </v-row>
