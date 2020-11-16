2e5c226433f33bfd1e244933f170493c55166e96
Daniel Peach <dpeaches96@gmail.com>
2020-11-16 14:10:47 -0500

    removed data call from Dashboard.vue

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -3,7 +3,6 @@
     <v-row>
       <v-col>
         <h2>Welcome to the Dashboard</h2>
-        <p>{{fetching_status}}</p>
         <v-btn
             block
             elevation="2"
@@ -32,18 +31,6 @@ import axios from 'axios';
 
 export default {
   name: 'Dashboard',
-  data: () => ({
-      fetching_status: "",
-  }),
-  created() {
-    axios.get(`/list/admins`)
-    .then(res => {
-      console.log(res);
-      this.fetching_status = res.data;
-    }).catch(err => {
-      console.log(err.response.data);
-      this.fetching_status = err.response.data;
-    });
-  }
+  data: () => ({}),
 }
 </script>
