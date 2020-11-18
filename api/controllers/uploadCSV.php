58ba9e6cf395d4dea2cf7e8279793c590f2034fd
Kate Davis <daviskate84@gmail.com>
2020-11-17 19:27:54 -0500

    not sure what to do next

--- api/controllers/uploadCSV.php
+++ api/controllers/uploadCSV.php
@@ -15,10 +15,20 @@ function uploadCSV(Slim\Slim $app) {
       $categories = [];
       $booths = [];
       while (($row = fgetcsv($file, 10000, ",")) !== FALSE) {
+
+        //assign vars to rows
         $firstName = $row[0];
         $lastName = $row[1];
         $schoolName = $row[2];
         $county = $row[3];
+        $gender = $row[4];
+        $projectNum = $row[5];
+        $title = $row[6];
+        $abstract = $row[7];
+        $gradeLevel = $row[8];
+        $category = $row[9];
+        $cnId = $row[10];
+        $boothNumber = $row[11];
 
         $existingSchool = false;
         foreach ($schools as $key => $value) {
@@ -33,7 +43,16 @@ function uploadCSV(Slim\Slim $app) {
       }
 
       fclose($file);
-
+      
+      //not sure where to put these
+      $booth = "INSERT INTO Booth(Number) VALUES(?)";
+      $count = "INSERT INTO County(CountyId, Name) VALUES(?, ?)";
+      $school= "INSERT INTO School(SchoolId, Name, CountyId) VALUES (?, ?, ?)";
+      $category = "INSERT INTO Category(CategoryId, Name) VALUES (?, ?)";
+      $gradeLevel = "INSERT INTO GradeLevel(GradeLevelId, Name) VALUES (?, ?)";
+      $project = "INSERT INTO Project(ProjectId, Number, Name, Abstract, BoothId, CourseNetworkingId, CategoryId) VALUES (?, ?, ?, ?, ?, ?, ?)";
+      $student = "INSERT INTO Student(UserId, SchoolId, ProjectId, GradeLevelId) VALUES (?, ?, ?, ?)";
+      
       echo "CSV File has been successfully Imported.";
   };
 }
